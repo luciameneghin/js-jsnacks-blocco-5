e805909e4d3983a6f17aa54bd93b8358e1189f1f
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-02 15:54:58 +0100

    feat: snack-8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcoCLass = students.find(student => {
+  return student.name === 'Marco Lanci'
+});
+console.log(marcoCLass.class);
