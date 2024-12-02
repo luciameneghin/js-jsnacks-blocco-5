79db5e4be84ff29a07316cb7dcf560f83f27e12c
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-02 15:43:52 +0100

    feat: snack-7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+const idDue = students.find(student => {
+  if (student.id === 2) {
+    return student
+  }
+});
+console.log(idDue);
